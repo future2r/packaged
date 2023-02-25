fedd0ef62795142dc7ac5171b78c36dafd50b24e
Frank Ulbricht <frank@ulbricht.name>
2023-02-25 13:11:34 +0100

    Create Windows installer package

--- CreatePackage.cmd
+++ CreatePackage.cmd
@@ -1,24 +1,50 @@
 @ECHO OFF
 REM Define the Java to use (adjust this to your environment).
 
-SET JAVA_HOME=C:/Program Files (Arm)/Microsoft/jdk-17.0.6.10-hotspot
+SET JAVA_HOME=C:\Program Files (Arm)\Microsoft\jdk-17.0.6.10-hotspot
 
 REM Build the project archive with Maven Wrapper.
 
-CALL mvnw clean package
+ECHO Creating application archive...
+CALL mvnw --quiet clean package
 
 REM Define some some variables
 SET APP_NAME=Packaged
+SET APP_VERSION=2.0.0
+SET APP_VENDOR=Frank Ulbricht
+SET APP_COPYRIGHT=Copyright (c) 2023 Frank Ulbricht
 SET MAIN_MODULE=name.ulbricht.packaged
-SET MODULE_PATH=target/module
-SET RELEASE_PATH=target/release
+SET MODULE_PATH=target\module
+SET RELEASE_PATH=target\release
+SET LICENSE_FILE=install\license.txt
 
 REM Create the application image (for a Windows console application).
 
+ECHO Creating application image...
 CALL "%JAVA_HOME%\bin\jpackage"^
  --type app-image^
  --win-console^
- --dest %RELEASE_PATH%^
  --name %APP_NAME%^
+ --app-version %APP_VERSION%^
+ --vendor "%APP_VENDOR%"^
+ --copyright "%APP_COPYRIGHT%"^
+ --dest %RELEASE_PATH%^
  --module-path %MODULE_PATH%^
  --module %MAIN_MODULE%
+
+ REM Create an application package from the pre-built application image
+ REM This requires WiX 3.0 or later from https://wixtoolset.org in the PATH
+
+ECHO Creating application package...
+CALL "%JAVA_HOME%\bin\jpackage"^
+ --type msi^
+ --app-version %APP_VERSION%^
+ --dest %RELEASE_PATH%^
+ --app-image %RELEASE_PATH%\%APP_NAME%^
+ --install-dir "%APP_VENDOR%\%APP_NAME%"^
+ --license-file %LICENSE_FILE%^
+ --win-dir-chooser^
+ --win-menu^
+ --win-menu-group %APP_NAME%^
+ --win-shortcut^
+ --win-shortcut-prompt^
